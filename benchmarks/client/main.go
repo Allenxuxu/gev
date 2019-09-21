97c7a615b66477ed2338bbc9b2f37516ff155bcd
Allenxuxu <120582243@qq.com>
2019-09-22 01:57:02 +0800

    增加压测客户端

---
+++ benchmarks/client/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"net"
+	"time"
+)
+
+var addr = flag.String("a", "localhost:1833", "address")
+var num = flag.Int("c", 1, "connection number")
+var timeOut = flag.Int("t", 2, "timeout second")
+var msgLen = flag.Int("m", 1024, "message length")
+
+var msg []byte
+
+func main() {
+	flag.Parse()
+	msg = make([]byte, *msgLen)
+	rand.Read(msg)
+
+	startC := make(chan interface{})
+	closeC := make(chan interface{})
+	result := make(chan int64, *num)
+
+	for i := 0; i < *num; i++ {
+		conn, err := net.Dial("tcp", *addr)
+		if err != nil {
+			panic(err)
+		}
+		go handler(conn, startC, closeC, result)
+	}
+
+	// start
+	close(startC)
+
+	time.Sleep(time.Duration(*timeOut) * time.Second)
+	// stop
+	close(closeC)
+
+	var totalMessagesRead int64
+	for i := 0; i < *num; i++ {
+		totalMessagesRead += <-result
+	}
+
+	fmt.Println("totalMessagesRead :", totalMessagesRead)
+	fmt.Println(totalMessagesRead*int64(*msgLen)/int64(*timeOut*1024*1024), " MiB/s throughput")
+}
+
+func handler(conn net.Conn, startC chan interface{}, closeC chan interface{}, result chan int64) {
+	var count int64
+	buf := make([]byte, 2048)
+	<-startC
+
+	_, e := conn.Write(msg)
+	if e != nil {
+		fmt.Println("Error to send message because of ", e.Error())
+	}
+
+	for {
+		select {
+		case <-closeC:
+			result <- count
+			conn.Close()
+			return
+		default:
+			n, err := conn.Read(buf)
+			if err != nil {
+				fmt.Print("Error to read message because of ", err)
+				return
+			}
+
+			_, err = conn.Write(buf[:n])
+			if err != nil {
+				fmt.Println("Error to send message because of ", e.Error())
+			}
+
+			count++
+		}
+	}
+}
