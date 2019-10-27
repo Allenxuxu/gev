2cef5ee3cfa0148f7854ac126fa88be6d77eefd8
Allenxuxu <120582243@qq.com>
2019-10-27 12:48:25 +0800

    use pool bytes

--- ws/read.go
+++ ws/read.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Allenxuxu/ringbuffer"
 	"github.com/Allenxuxu/toolkit/convert"
+	"github.com/gobwas/pool/pbytes"
 )
 
 // Errors used by frame reader.
@@ -22,7 +23,9 @@ func VirtualReadHeader(in *ringbuffer.RingBuffer) (h Header, err error) {
 		return
 	}
 
-	bts := make([]byte, 2, MaxHeaderSize-2)
+	// bts := make([]byte, 2, MaxHeaderSize-2)
+	bts := pbytes.Get(2, MaxHeaderSize-2)
+	defer pbytes.Put(bts)
 	// Prepare to hold first 2 bytes to choose size of next read.
 	_, _ = in.VirtualRead(bts)
 
