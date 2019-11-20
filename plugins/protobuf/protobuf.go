18bc085808da3921025e4589d4b8221322250a2b
Allenxuxu <120582243@qq.com>
2019-11-20 20:04:55 +0800

    add protobuf PackMessage methd

--- plugins/protobuf/protobuf.go
+++ plugins/protobuf/protobuf.go
@@ -1,1 +1,17 @@
 package protobuf
+
+import "encoding/binary"
+
+func PackMessage(msgType string, data []byte) []byte {
+	typeLen := len(msgType)
+	len := len(data) + typeLen + 2
+
+	ret := make([]byte, len+4)
+
+	binary.BigEndian.PutUint32(ret, uint32(len))
+	binary.BigEndian.PutUint16(ret[4:], uint16(typeLen))
+	copy(ret[6:], msgType)
+	copy(ret[6+typeLen:], data)
+
+	return ret
+}
