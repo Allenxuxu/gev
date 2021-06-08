80dcbd580e32723474364bb56b0462d9c51e87a0
徐旭 <120582243@qq.com>
2021-06-08 01:32:05 +0000

    renaame len -> length

--- plugins/protobuf/protobuf.go
+++ plugins/protobuf/protobuf.go
@@ -5,11 +5,11 @@ import "encoding/binary"
 // PackMessage 按自定义协议打包数据
 func PackMessage(msgType string, data []byte) []byte {
 	typeLen := len(msgType)
-	len := len(data) + typeLen + 2
+	length := len(data) + typeLen + 2
 
-	ret := make([]byte, len+4)
+	ret := make([]byte, length+4)
 
-	binary.BigEndian.PutUint32(ret, uint32(len))
+	binary.BigEndian.PutUint32(ret, uint32(length))
 	binary.BigEndian.PutUint16(ret[4:], uint16(typeLen))
 	copy(ret[6:], msgType)
 	copy(ret[6+typeLen:], data)
